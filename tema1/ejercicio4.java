2ceb0697fb5b1e0fe8487f27d9587f69cc39808c
mpermor <mpermor419@g.educaand.es>
2026-09-22 14:26:06 +0200

    Add files via upload

---
+++ tema1/ejercicio4.java
@@ -0,0 +1,14 @@
+public class ejercicio4 {
+	public static void main(String[] args) {
+		System.out.println("Hello\tHola");
+		System.out.println("Sun\tSol");
+		System.out.println("Tree\tÁrbol");
+		System.out.println("Table\tMesa");
+		System.out.println("Game\tJuego");
+		System.out.println("Apple\tManzana");
+		System.out.println("Potato\tPatata");
+		System.out.println("Dog\tPerro");
+		System.out.println("Cat\tGato");
+		System.out.println("Fox\tZorro");
+	}
+}
