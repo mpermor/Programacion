528e18bb15972350ccbc0fdb0de2f45a09946dc9
mpermor <mpermor419@g.educaand.es>
2026-09-22 14:15:36 +0200

    Add files via upload

--- tema1/ejercicio1.java
+++ tema1/ejercicio1.java
@@ -1,4 +1,4 @@
-public class SalidaFormateada02 {
+public class ejercicio1 {
 	public static void main(String[] args) {
 		System.out.println("  Artículo    Precio/caja    Nº cajas");
 		System.out.println("--------------------------------------");
---
+++ tema1/ejercicio3.java
@@ -0,0 +1,7 @@
+public class ejercicio3 {
+	public static void main(String[] args) {
+		System.out.println("Marcos");
+		System.out.println("Calle Java 123");
+		System.out.println("123 456 789");
+	}
+}
