c57c7a2e675066866d81c21c862100b976d4cca0
mpermor <mpermor419@g.educaand.es>
2026-09-25 13:26:30 +0200

    Add files via upload

---
+++ tema1/ejercicio5.java
@@ -0,0 +1,12 @@
+public class ejercicio5 {
+	public static void main(String[] args) {
+		System.out.println("Lunes\tMartes\tMiércoles\tJueves\tViernes");
+		System.out.println("===============================================");
+		System.out.println("LMSGI\tBADAT\tSIINF\t\tPROGR\tENDES");
+		System.out.println("LMSGI\tBADAT\tSIINF\t\tPROGR\tBADAT");
+		System.out.println("SIINF\tSASP\tIPE\t\tLMSGI\tBADAT");
+		System.out.println("SIINF\tENDES\tIPE\t\tSIINF\tIPE");
+		System.out.println("PROGR\tPROGR\tDASPGS\t\tBADAT\tPROGR");
+		System.out.println("PROGR\tPROGR\tENDES\t\tBADAT\tPROGR");
+	}
+}
